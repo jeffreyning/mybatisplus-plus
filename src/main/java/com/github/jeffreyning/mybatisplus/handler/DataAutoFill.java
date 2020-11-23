34124a5bb0c406b5cf1ccb19aed215e18acf7a07
ninghao <jeffreyning@aliyun.com>
2020-11-23 19:58:38 +0800

    修改异常信息

--- src/main/java/com/github/jeffreyning/mybatisplus/handler/DataAutoFill.java
+++ src/main/java/com/github/jeffreyning/mybatisplus/handler/DataAutoFill.java
@@ -1,7 +1,6 @@
 package com.github.jeffreyning.mybatisplus.handler;
 
 import java.lang.reflect.Field;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -17,90 +16,83 @@ import com.github.jeffreyning.mybatisplus.anno.InsertFill;
 import com.github.jeffreyning.mybatisplus.anno.UpdateFill;
 
 
-
-
 @Component
 public class DataAutoFill implements MetaObjectHandler {
-@Autowired
-private SqlSessionTemplate sqlSessionTemplate;
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
 
-@Override
-public void insertFill(MetaObject metaObject) {
-    	Object classObj = metaObject.getOriginalObject();
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        Object classObj = metaObject.getOriginalObject();
         Field[] declaredFields = classObj.getClass().getDeclaredFields();
-        if(declaredFields!=null){
-            for(Field field:declaredFields){
+        if (declaredFields != null) {
+            for (Field field : declaredFields) {
 
                 InsertFill insertFill = field.getAnnotation(InsertFill.class);
-                if(insertFill!=null ){
-                	String fieldName=field.getName();
-                	String sql=insertFill.value();
-                	if(sql!=null && !"".equals(sql)) {
-                	SqlSession sqlSession=null;
-					try {
-						sqlSession=SqlSessionUtils.getSqlSession(
-								sqlSessionTemplate.getSqlSessionFactory(), sqlSessionTemplate.getExecutorType(),
-								sqlSessionTemplate.getPersistenceExceptionTranslator());
-						ResultSet resultSet = sqlSession.getConnection().createStatement().executeQuery(sql);
-						if(resultSet!=null) {
-							if(resultSet.next()) {
-							Object fieldVal=resultSet.getObject(1);
-							this.setFieldValByName(fieldName, fieldVal, metaObject);
-							}
-						}
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}finally {
-						if(sqlSession!=null) {
-							SqlSessionUtils.closeSqlSession(sqlSession, sqlSessionTemplate.getSqlSessionFactory());
-						}
-					}
-                	}
+                if (insertFill != null) {
+                    String fieldName = field.getName();
+                    String sql = insertFill.value();
+                    if (sql != null && !"".equals(sql)) {
+                        SqlSession sqlSession = null;
+                        try {
+                            sqlSession = SqlSessionUtils.getSqlSession(
+                                    sqlSessionTemplate.getSqlSessionFactory(), sqlSessionTemplate.getExecutorType(),
+                                    sqlSessionTemplate.getPersistenceExceptionTranslator());
+                            ResultSet resultSet = sqlSession.getConnection().createStatement().executeQuery(sql);
+                            if (resultSet != null) {
+                                if (resultSet.next()) {
+                                    Object fieldVal = resultSet.getObject(1);
+                                    this.setFieldValByName(fieldName, fieldVal, metaObject);
+                                }
+                            }
+                        } catch (SQLException e) {
+                            throw new RuntimeException("insert fill error", e);
+                        } finally {
+                            if (sqlSession != null) {
+                                SqlSessionUtils.closeSqlSession(sqlSession, sqlSessionTemplate.getSqlSessionFactory());
+                            }
+                        }
+                    }
 
                 }
-                
+
             }
         }
 
-        }
+    }
 
     @Override
-public void updateFill(MetaObject metaObject) {
-    	Object classObj = metaObject.getOriginalObject();
+    public void updateFill(MetaObject metaObject) {
+        Object classObj = metaObject.getOriginalObject();
         Field[] declaredFields = classObj.getClass().getDeclaredFields();
-        if(declaredFields!=null){
-            for(Field field:declaredFields){
-
+        if (declaredFields != null) {
+            for (Field field : declaredFields) {
                 UpdateFill updateFill = field.getAnnotation(UpdateFill.class);
-                if(updateFill!=null ){
-                	String fieldName=field.getName();
-                	String sql=updateFill.value();
-                	if(sql!=null && !"".equals(sql)) {
-                	SqlSession sqlSession=null;
-					try {
-						sqlSession=SqlSessionUtils.getSqlSession(
-								sqlSessionTemplate.getSqlSessionFactory(), sqlSessionTemplate.getExecutorType(),
-								sqlSessionTemplate.getPersistenceExceptionTranslator());
-						ResultSet resultSet = sqlSession.getConnection().createStatement().executeQuery(sql);
-						if(resultSet!=null) {
-							if(resultSet.next()) {
-							Object fieldVal=resultSet.getObject(1);
-							this.setFieldValByName(fieldName, fieldVal, metaObject);
-							}
-						}
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}finally {
-						if(sqlSession!=null) {
-							SqlSessionUtils.closeSqlSession(sqlSession, sqlSessionTemplate.getSqlSessionFactory());
-						}
-					}
-                	}
-
+                if (updateFill != null) {
+                    String fieldName = field.getName();
+                    String sql = updateFill.value();
+                    if (sql != null && !"".equals(sql)) {
+                        SqlSession sqlSession = null;
+                        try {
+                            sqlSession = SqlSessionUtils.getSqlSession(
+                                    sqlSessionTemplate.getSqlSessionFactory(), sqlSessionTemplate.getExecutorType(),
+                                    sqlSessionTemplate.getPersistenceExceptionTranslator());
+                            ResultSet resultSet = sqlSession.getConnection().createStatement().executeQuery(sql);
+                            if (resultSet != null) {
+                                if (resultSet.next()) {
+                                    Object fieldVal = resultSet.getObject(1);
+                                    this.setFieldValByName(fieldName, fieldVal, metaObject);
+                                }
+                            }
+                        } catch (SQLException e) {
+                            throw new RuntimeException("update fill error", e);
+                        } finally {
+                            if (sqlSession != null) {
+                                SqlSessionUtils.closeSqlSession(sqlSession, sqlSessionTemplate.getSqlSessionFactory());
+                            }
+                        }
+                    }
                 }
-                
             }
         }
     }
